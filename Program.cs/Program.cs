5a0302a40517d16306550d75e9a0340391abfb9b
Kevin Wilusz <kwilusz10@gmail.com>
2022-08-29 12:38:53 +0200

    small problems on the way of making the httpclient awaiting, error: httpclient laks await capabilities

--- Program.cs/Program.cs
+++ Program.cs/Program.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net.Http;
+using HtmlAgilityPack;
+using System.Net.Http.Headers;
+using System.Net;
+using System.IO;
 
 
 namespace Program.cs
@@ -11,10 +15,36 @@ namespace Program.cs
     internal class Program
     {
         
-        public static void Main(string[] args)
+        public static async void Main(string[] args)
         {
             Console.WriteLine("we are ready to go!");
             Console.WriteLine("send the link to website u wanna scrape or the file");
+            string link = Console.ReadLine();
+
+            HttpClient client = new HttpClient(); 
+            HttpRequestMessage request =  new HttpRequestMessage(HttpMethod.Get, link);
+            var content =  request.Content; 
+            
+            if (content == null)  //Is crashing here
+            {
+                Console.WriteLine("Dang it! we could'nt find anything");
+
+            }
+           else
+            {
+                Console.WriteLine(content);
+                Console.ReadLine();
+                
+            }
+
+            Console.WriteLine("test 2");
+            
+
+            
+
+
+
+
             
 
 
